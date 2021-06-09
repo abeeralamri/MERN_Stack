6f32af055b7f27f3102cfc40276904c3fae887fb
Abeer Alamri <48870279+abeeralamri@users.noreply.github.com>
2021-06-10 00:08:46 +0300

    Delete Full_Stack/ProductManager_part3 directory

--- Full_Stack/ProductManager_part3/server/config/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-
-mongoose.connect(`mongodb://localhost/product`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log(`Connected to Mongo Database: Product`))
-    .catch( err => console.log("Didn't successfully connect...", err));
